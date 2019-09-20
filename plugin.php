7af56a1a9ca2322852755763cf0dfae8f02d0735
Max Kostikov <max@kostikov.co>
2019-09-20 10:34:23 +0200

    Remove Google Plus button

--- plugin.php
+++ plugin.php
@@ -13,7 +13,6 @@ class plugina2a extends Plugin {
 			<div class="a2a_kit a2a_kit_size_32 a2a_default_style a2a_specialcss">
 				<a class="a2a_button_facebook"></a>
 				<a class="a2a_button_twitter"></a>
-				<a class="a2a_button_google_plus"></a>
 				<a class="a2a_button_linkedin"></a>
 				<a class="a2a_button_mastodon"></a>
 				<a class="a2a_dd" href="https://www.addtoany.com/share"></a>
